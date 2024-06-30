b5d5ecb999360cbc6bd3db4f3469b432fe28b43c
Code Monkey King <freesoft.for.people@gmail.com>
2024-06-30 12:53:46 +0300

    implemented urgent moves

--- microgo.c
+++ microgo.c
@@ -23,6 +23,31 @@ for(int i=0;i<I;i++){if(b[i]<='.')continue;if((b[i]-'`')&(3-(c-'`'))){
 C(i,3-(c-'`'));if(!e){if(r==1&&Y(q)==3-s)k=g[0];for(int j=0;j<r;j++)
 b[g[j]]='.';}R();}}s=3-s;}
 
+int search(int depth) { /* Recursively search fighting moves */
+if (!depth) return 0; int bestScore = -10000; int u[100];
+memset(u,0,100*sizeof(int));int y=0; for (int q=0;q<I;q++){
+if(b[q]<=' '||b[q]=='.')continue;C(q,b[q]);if(e<3){for(int j=0;j<e;j++)
+{int f=0;for(int z=0;z<y;z++)if(u[z]==l[j])f=1;if(!f)u[y++]=l[j];}}R();}
+for (int i=0; i <y;i++)printf("urgent %d\n", u[i]);
+  
+  /*for (let sq of getUrgentMoves()) {
+    for (let offset of [1, -1, size, -size])
+      if (board[sq+offset] == OFFBOARD && depth == 1) continue;
+    if (sq == ko) continue;
+    let oldBoard = JSON.stringify(board);
+    let oldSide = side;
+    let oldKo = ko;
+    if (!setStone(sq, side, false)) continue;
+    let eval = -search(depth-1);
+    if (eval > bestScore) {
+      bestScore = eval;
+      bestMove = sq;
+    } board = JSON.parse(oldBoard);
+    side = oldSide;
+    ko = oldKo;
+  };return bestScore;*/
+}
+
 void D(){setbuf(stdin,NULL);setbuf(stdout,NULL);char u[10000];while(1){ // GTP communication
 memset(u,0,sizeof(u));fflush(stdout);if(!fgets(u,10000,stdin))continue;
 if(u[0]=='\n')continue;if(strncmp(u,"name",4)==0)printf("= Micro Go\n");
@@ -45,11 +70,16 @@ void debug() {
   P(3*S+6,'b');
   P(16*S+16,'a');
   P(3*S+4,'b');
-  //P(3*S+5,'a');
+  P(3*S+7,'a');
+  P(16*S+4,'b');
+  P(2*S+6,'a');
+  P(16*S+3,'b');
+  P(4*S+6,'a');
+  search(1);
   printf("%s %d\n", b,k);
 }
 
 int main() {
-  D();
-  //debug();
+  //D();
+  debug();
 }
