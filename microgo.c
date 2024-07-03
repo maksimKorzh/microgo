097558d47af5654c008dd6bb969c6238dc6abb41
Code Monkey King <freesoft.for.people@gmail.com>
2024-07-03 22:22:59 +0300

    more less done

--- microgo.c
+++ microgo.c
@@ -18,7 +18,7 @@ o=3-c;}else if(((b[q+n[i]]-'`')&3)==o)return 0;}return c;}
 int P(int q,int c){if(b[q]!='.'||q==k)return 0;int _k=k;k=E; b[q]=c;              // Put stone on board, handle captures
 for(int i=0;i<I;i++){if(b[i]<='.')continue;if((b[i]-'`')&(3-(c-'`'))){
 C(i,3-(c-'`'));if(!e){if(r==1&&Y(q)==3-s)k=g[0];for(int j=0;j<r;j++)
-b[g[j]]='.';}R();}}C(q,c-'`');    int suicide=e?0:1;R();if(suicide){b[q]='.';
+b[g[j]]='.';}R();}}C(q,c-'`');int suicide=e?0:1;R();if(suicide){b[q]='.';
 k=_k;return 0;}s=3-s;return 1;}
 int X(int x){if(!x){int v=0,a=0,w=0;for(int q=0;q<I;q++){                         // Recursively search fighting moves at depth 6
 if(b[q]<=' '||b[q]=='.')continue;if(b[q]=='a')a+=1;if(b[q]=='b')
@@ -26,14 +26,12 @@ w+=1;}v+=(a-w);return(s==B)?v:-v;};int h=-10000;int u[100];
 memset(u,0,100*sizeof(int));int y=0;for(int q=0;q<I;q++){
 if(b[q]<=' '||b[q]=='.')continue;C(q,b[q]-'`');if(e<3){for(int j=0;j<e;j++)
 {int f=0;for(int z=0;z<y;z++)if(u[z]==l[j])f=1;if(!f)u[y++]=l[j];}}R();}
-for(int q=0;q<(y<6?y:5);q++){if(u[q]==k)continue;char _b[]=G;strcpy(_b,b);
+for(int q=0;q<(y<7?y:6);q++){if(u[q]==k)continue;char _b[]=G;strcpy(_b,b);
 int _s=s;int _k=k;if(!P(u[q],s+'`'))continue;int p=-X(x-1);
 if(p>h){h=p;if(x==6) /* best move */ m=u[q];}strcpy(b,_b);s=_s;k=_k;}return h;}
 void T(){for(int q=0;q<17;q++){if(b[d[q]]=='.'&&!(Y(d[q]))){m=d[q];return;}}      // Play away if no forcing moves
-for(int q=0;q<I;q++){if((b[q]-'`')==3-s){for(int i=0;i<4;i++){
-if(b[q+n[i]]=='.'){if(b[q+n[i]*2]<' '||b[q+n[i]*3]<' ')break;
-int c=0;for(int j=0;j<4;j++)if(b[q+n[i]+n[j]]=='.')c++;
-if(c>1)m=q+n[i];break;}}}}}
+for(int q=0;q<I;q++){if((b[q]-'`')==3-s){int a=0;C(q,b[q]-'`');if(e>2)a=l[0];
+R();int c=0;for(int j=0;j<4;j++)if(b[a+n[j]]=='.')c++;if(a&&c){m=a;break;}}}}
 void D(){setbuf(stdin,NULL);setbuf(stdout,NULL);char u[10000];while(1){           // GTP communication
 memset(u,0,sizeof(u));fflush(stdout);if(!fgets(u,10000,stdin))continue;
 if(u[0]=='\n')continue;if(strncmp(u,"name",4)==0)printf("= Micro Go\n");
@@ -47,5 +45,4 @@ char c[20];int y=(m/21)-1,x=(m%21)-1;c[0]='A'+x+(x>=8);sprintf(c+1,"%d",19-y);
 c[3]='\0';c[0]='A'+x+(x>=8);printf("= %s\n\n",c);}
 else if(strncmp(u,"play",4)==0){int c=u[5]=='B'?'a':'b';int x=u[7]-'A'+1-(u[7]>'I'?1:0);
 int y;sscanf(u,"play %*c %*c%d",&y);y=S-1-y;P(y*S+x,c);printf("=\n\n");}
-else if(strncmp(u,"quit",4)==0)break;else printf("=\n\n");}}
-int main(){D();}
+else if(strncmp(u,"quit",4)==0)break;else printf("=\n\n");}}int main(){D();}
